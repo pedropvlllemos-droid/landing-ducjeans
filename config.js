5fd039778df0529baafead4812e23e57680305a7
pedropvlllemos-droid <pedrop.vll.lemos@gmail.com>
2026-09-18 14:50:49 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.DUC_CONFIG = {
   // Depois de publicar o Google Apps Script como Web App, cole aqui a URL que termina em /exec.
-  APPS_SCRIPT_URL: '',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbz9OqOhWfr3okUx3zZ62iinpjSjKTZ0jMF3axQGoHSQxFuvKlo3-7MWR46soMSdtlk/exec',
 
   // Opcional: ID do Meta Pixel. Deixe vazio enquanto não estiver configurado.
   META_PIXEL_ID: '',
